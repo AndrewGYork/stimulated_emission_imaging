449399a7acd7533b83a2e29261b4b6374289af49
sanjayvijay <chettah@gmail.com>
2016-12-21 14:07:10 -0800

    Add files via upload

--- javascript/update_figures.js
+++ javascript/update_figures.js
@@ -13,3 +13,11 @@ function update_figure_6() {
   var image = document.getElementById("Figure_6_image");
   image.src = filename;
 }
+
+// Figure 7 interactively loads static images, stored locally
+function update_figure_7() {
+  var angle = document.getElementById("Figure_7_angle").value;
+  var filename = "./images/figure_7/phase_STE_image_" + angle + ".svg";
+  var image = document.getElementById("Figure_7_image");
+  image.src = filename;
+}
