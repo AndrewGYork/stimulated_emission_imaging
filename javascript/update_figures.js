b0a6c2a93f7ac194c761f20192dc1fa1229e1aeb
Sanjay Ramesh Varma <chettah@gmail.com>
2017-07-19 15:43:36 -0700

    Update update_figures.js
    Interactive figure for experimental setup (Figure 2)

--- javascript/update_figures.js
+++ javascript/update_figures.js
@@ -1,3 +1,15 @@
+// Figure 2 interactively loads static images, stored locally
+function update_figure_2() {
+  var sample_type = document.getElementById("Figure_2_sample_type").value;
+  var imaging_modality = document.getElementById("Figure_2_imaging_modality").value;
+  var filename = "./images/figure_2/" + sample_type + "_" + imaging_modality + ".svg";
+  var image = document.getElementById("Figure_2_image");
+  image.src = filename;
+}
+
+
+
+
 // Figure 5 interactively loads static images, stored locally
 function update_figure_5() {
   var z = document.getElementById("Figure_5_z").value;
