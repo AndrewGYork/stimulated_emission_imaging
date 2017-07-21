743bfa979ee316f3b95860b32661e664ec2aabae
Sanjay Ramesh Varma <chettah@gmail.com>
2017-07-20 18:59:46 -0700

    Update update_figures.js

--- javascript/update_figures.js
+++ javascript/update_figures.js
@@ -16,11 +16,19 @@ function update_figure_4() {
 }
 
 
-// Figure 5 interactively loads static images, stored locally
-function update_figure_5() {
+// Figure 5ab interactively loads static images, stored locally
+function update_figure_5ab() {
   var z = document.getElementById("Figure_5_z").value;
   var filename = "./images/figure_5/darkfield_STE_image_" + z + ".svg";
-  var image = document.getElementById("Figure_5_image");
+  var image = document.getElementById("Figure_5ab_image");
+  image.src = filename;
+}
+
+// Figure 5cd interactively loads static images, stored locally
+function update_figure_5cd() {
+  var view = document.getElementById("Figure_5_view").value;
+  var filename = "./images/figure_5/darkfield_STE_image_" + view + ".svg";
+  var image = document.getElementById("Figure_5cd_image");
   image.src = filename;
 }
 
