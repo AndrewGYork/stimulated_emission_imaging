98f7b2547b6602358d822b78a9eb57d75f1f7eff
Sanjay Ramesh Varma <chettah@gmail.com>
2017-07-27 14:49:11 -0700

    Update update_figures.js

--- javascript/update_figures.js
+++ javascript/update_figures.js
@@ -34,16 +34,8 @@ function update_figure_5cd() {
 
 // Figure 6 interactively loads static images, stored locally
 function update_figure_6() {
-  var z = document.getElementById("Figure_6_z").value;
-  var filename = "./images/figure_6/fluorescence_depletion_image_" + z + ".svg";
+  var angle = document.getElementById("Figure_6_angle").value;
+  var filename = "./images/figure_6/phase_STE_image_" + angle + ".svg";
   var image = document.getElementById("Figure_6_image");
-  image.src = filename;
-}
-
-// Figure 7 interactively loads static images, stored locally
-function update_figure_7() {
-  var angle = document.getElementById("Figure_7_angle").value;
-  var filename = "./images/figure_7/phase_STE_image_" + angle + ".svg";
-  var image = document.getElementById("Figure_7_image");
   image.src = filename;
 }
