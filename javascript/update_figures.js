6bd409820537f600df00a2c940a7e5f35a1fe86b
Sanjay Ramesh Varma <chettah@gmail.com>
2017-12-08 16:28:44 -0800

    Update update_figures.js

--- javascript/update_figures.js
+++ javascript/update_figures.js
@@ -29,8 +29,8 @@ function update_figure_1c() {
 // Figure 2 interactively loads static images, stored locally
 function update_figure_2() {
   var sample_type = document.getElementById("Figure_2_sample_type").value;
-  var imaging_modality = document.getElementById("Figure_2_imaging_modality").value;
-  var filename = "./images/figure_2/" + sample_type + "_" + imaging_modality + ".svg";
+  var imaging_method = document.getElementById("Figure_2_imaging_method").value;
+  var filename = "./images/figure_2/" + sample_type + "_" + imaging_method + ".svg";
   var image = document.getElementById("Figure_2b_image");
   image.src = filename;
 }
