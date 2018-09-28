7f6140206857b0ef1dad93faf7b5ae0a7e58a177
Sanjay Ramesh Varma <chettah@gmail.com>
2018-09-28 13:14:54 -0700

    Add files via upload

---
+++ figure_generation/Figure_1c/dark_field_scatter_only/animation_generator_mp4_0.py
@@ -0,0 +1,31 @@
+import os
+from subprocess import check_call
+
+
+print("Converting to mp4...", end='')
+convert_command = [
+    'ffmpeg', '-y',
+    '-r', '10',
+    '-f', 'image2',
+    '-i', 'img%d.png',
+    '-movflags', 'faststart',
+    # See here: https://trac.ffmpeg.org/wiki/Encode/H.264#Encodingfordumbplayers
+    '-pix_fmt', 'yuv420p',
+    '-vcodec', 'libx264',
+    '-vf', 'scale=trunc(iw/2)*2:trunc(ih/2)*2', # Ensures divisible by 2
+    '-preset', 'veryslow',
+    '-crf', '25',
+    'fig_1c_video.mp4']
+print(convert_command)
+try:
+    with open('conversion_messages.txt', 'wt') as f:
+        f.write("So far, everthing's fine...\n")
+        f.flush()
+        check_call(convert_command, stderr=f, stdout=f)
+        f.flush()
+    os.remove('conversion_messages.txt')
+except: # This is unlikely to be platform independent :D
+    print("MP4 conversion failed. Is ffmpeg installed?")
+    raise
+print('done.')
+
