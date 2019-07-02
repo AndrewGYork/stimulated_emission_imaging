32f514eb45deae4d089a3d7157873d9797c8bb81
Sanjay Ramesh Varma <chettah@gmail.com>
2019-07-01 17:05:34 -0700

    Add files via upload

--- figure_generation/figure_A6.py
+++ figure_generation/figure_A6.py
@@ -1,235 +1,206 @@
 import os
 import numpy as np
-import matplotlib.pyplot as plt
-import np_tif
 from stack_registration import bucket
+import matplotlib.pyplot as plt
+from matplotlib.ticker import FuncFormatter
+import matplotlib.patches as patches
+import np_tif
 
 def main():
-
-    # the data to be plotted by this program is generated from raw tifs
-    # and repetition_average_expt_and_control.py
 
     assert os.path.isdir('./../images')
     if not os.path.isdir('./../images/figure_A6'):
         os.mkdir('./../images/figure_A6')
 
-    filename = (
-        './../../stimulated_emission_imaging-data' +
-        '/2017_08_25_STE_phase_thermal_nanodiamond_4_5x_longest_delays' +
-        '/STE_phase_angle_10_green_1350mW_red_300mW.tif')
-    filename_ctrl = (
-        './../../stimulated_emission_imaging-data' +
-        '/2017_08_25_STE_phase_thermal_nanodiamond_4_5x_longest_delays' +
-        '/STE_phase_angle_10_green_0mW_red_300mW.tif')
+    data_path = ('./../../stimulated_emission_imaging-data' +
+                 '/2016_11_18_modulated_imaging_darkfield_nanodiamond_7' +
+                 '_extra_green_filter/')
+
+    num_reps = 200 # number of times a power/delay stack was taken
+    num_delays = 5
+
+    # power calibration
+    # red max power is 300 mW
+    # green max power is 1450 mW
+    # green powers calibrated using camera
+    green_max_mW = 1450
+    green_powers = np.array(
+        (113.9,119.6,124.5,135,145.5,159.5,175.3,193.1,234.5,272.2,334.1,385.7,446.1))
+    # 0th power is AOM with 0 volts
+    green_powers = green_powers - min(green_powers)
+    green_powers = green_powers * green_max_mW / max(green_powers)
+    green_powers = np.around(green_powers).astype(int)
+    sparse_green_nums = [0,7,10,12]
+    green_powers_sparse = green_powers[sparse_green_nums]
+
+    # red powers calibrated using camera
+    red_max_mW = 300
+    red_powers = np.array(
+        (26.6, 113, 198, 276, 353, 438, 537))
+    # 0th power is AOM with 0 volts
+    red_powers = red_powers - min(red_powers)
+    red_powers = red_powers * red_max_mW / max(red_powers)
+    red_powers = np.around(red_powers).astype(int)
+    sparse_red_nums = [0,3,6]
+    red_powers_sparse = red_powers[sparse_red_nums]
+
+    # load representative images
+    STE_image_filename = (data_path + 'STE_image_avg.tif')
+    STE_image = np_tif.tif_to_array(STE_image_filename).astype(np.float64)
+    STE_image_bg_filename = (data_path + 'STE_image_bg_avg.tif')
+    STE_image_bg = np_tif.tif_to_array(STE_image_bg_filename).astype(np.float64)
+    darkfield_image_filename = (data_path + 'darkfield_image_avg.tif')
+    darkfield_image = np_tif.tif_to_array(
+        darkfield_image_filename).astype(np.float64)
+    darkfield_image_bg_filename = (data_path + 'darkfield_image_bg_avg.tif')
+    darkfield_image_bg = np_tif.tif_to_array(
+        darkfield_image_bg_filename).astype(np.float64)
+    STE_delta_image = STE_image - darkfield_image
+    STE_delta_image_bg = STE_image_bg - darkfield_image_bg
+    # crosstalk correction
+    STE_delta_image_corrected = STE_delta_image - STE_delta_image_bg
+
+    # load space-averaged signal data
+    filename = (data_path + 'signal_all_scaled.tif')
     data = np_tif.tif_to_array(filename).astype(np.float64)
-    data_ctrl = np_tif.tif_to_array(filename_ctrl).astype(np.float64)
-
-    # get rid of overexposed rows at top and bottom of images
-    less_rows = 3
-    data = data[:,0+less_rows:data.shape[1]-less_rows,:]
-    data_ctrl = data_ctrl[:,0+less_rows:data_ctrl.shape[1]-less_rows,:]
-
-    # Get the average pixel brightness in the background region of the
-    # phase contrast image. We'll use it to account for laser intensity
-    # fluctuations
-    avg_laser_brightness = get_bg_level(data.mean(axis=0))
+    bg_filename = (data_path + 'signal_green_blocked_all_scaled.tif')
+    bg = np_tif.tif_to_array(bg_filename).astype(np.float64)
     
-    # scale all images to have the same background brightness. This
-    # amounts to a correction of roughly 1% or less
-    local_laser_brightness = get_bg_level(data)
-    data = data * (
-        avg_laser_brightness / local_laser_brightness).reshape(
-            data.shape[0], 1, 1)
-    # do the same for control (green off) data
-    local_laser_brightness_ctrl = get_bg_level(data_ctrl)
-    data_ctrl = data_ctrl * (
-        avg_laser_brightness / local_laser_brightness_ctrl).reshape(
-            data_ctrl.shape[0], 1, 1)
+    # reshape to hyperstack
+    data = data.reshape((
+        num_reps,
+        len(red_powers),
+        len(green_powers),
+        num_delays,
+        ))
+    bg = bg.reshape((
+        num_reps,
+        len(red_powers),
+        len(green_powers),
+        num_delays,
+        ))
 
-    # get delays from code copied from
-    # modulated_imaging_one_z_STE_phase_thermal.py
-    num_reps = 50
-    daq_rate = 8e5
-    red_pulse_duration_pixels = 4
-    green_pulse_duration_pixels = 20
-    red_beginning_pixels = np.array([
-        -10,
-        0,
-        30,
-        50,
-        100,
-        150,
-        ])
-    red_step_pixels = 200
-    red_last_pixel = 7200
-    red_end_pixels = np.arange(
-        red_step_pixels, red_last_pixel, red_step_pixels)
-    red_start_pixel_array = np.append(red_beginning_pixels, red_end_pixels)
-    red_delays = red_start_pixel_array/daq_rate
-    red_pulse_duration = red_pulse_duration_pixels/daq_rate
-    green_pulse_duration = green_pulse_duration_pixels/daq_rate
-    
-    num_delays = red_delays.shape[0]
+    # compute STE signal
+    # (delay #2 -> 0 us delay, delay #0 and #4 -> +/- 2.5 us delay)
+    STE_signal = data[:,:,:,2] - 0.5*(data[:,:,:,0] + data[:,:,:,4])
+    STE_signal_bg = bg[:,:,:,2] - 0.5*(bg[:,:,:,0] + bg[:,:,:,4])
 
-    # reshape data by reps
-    data = data.reshape(
-        num_reps, num_delays, data.shape[1], data.shape[2])
-    data_ctrl = data_ctrl.reshape(
-        num_reps, num_delays, data_ctrl.shape[1], data_ctrl.shape[2])
-    bg_signal = local_laser_brightness.reshape(num_reps, num_delays)
+    # crosstalk correction
+    STE_signal_corrected = STE_signal - STE_signal_bg
 
-    # now that brightness is corrected, repetition average the data
-    data = data.mean(axis=0)
-    data_ctrl = data_ctrl.mean(axis=0)
-    bg_signal = bg_signal.mean(axis=0)
+    # remove bad repetition(s)
+    STE_signal_corrected = np.delete(STE_signal_corrected, 188, 0)
+    STE_signal_corrected = np.delete(STE_signal_corrected, 185, 0)
+    STE_signal_corrected = np.delete(STE_signal_corrected, 148, 0)
+    STE_signal_corrected = np.delete(STE_signal_corrected, 63, 0)
+    STE_signal_corrected = np.delete(STE_signal_corrected, 5, 0)
+    # also for regular signal in order to compute correct standard
+    # deviation without outliers
+    STE_signal = np.delete(STE_signal, 188, 0)
+    STE_signal = np.delete(STE_signal, 185, 0)
+    STE_signal = np.delete(STE_signal, 148, 0)
+    STE_signal = np.delete(STE_signal, 63, 0)
+    STE_signal = np.delete(STE_signal, 5, 0)
 
-    # The first delay is negative. Subtract this from the rest of the
-    # data in order to find the time delayed change in the phase
-    # contrast image due to the excitation pulse.
-    early_data = np.array([data[0,:,:]]*num_delays) # red precedes green
-    thermal_stack = data - early_data
-    early_data_ctrl = np.array([data_ctrl[0,:,:]]*num_delays) # red precedes green
-    crosstalk_stack = data_ctrl - early_data_ctrl
-    bg_signal_change = bg_signal - bg_signal[0]
-    # subtract AOM effects (even though it doesn't seem like there are any)
-    thermal_stack = thermal_stack# - crosstalk_stack
+    # standard deviation
+    STE_signal_std = np.std(STE_signal,axis=0)
 
-    # plot phase contrast image and thermal signal
-    top = 0
-    bot = 98
-    left = 94
-    right = 252
-    thermal_cropped = thermal_stack[:,top:bot,left:right]
+    # repetition average
+    STE_signal_corrected = STE_signal_corrected.mean(axis=0)
 
-    # Our pixels are tiny (8.7 nm/pixel) to give large dynamic range.
-    # This is not great for viewing, because fluctuations can swamp the
-    # signal. This step bins the pixels into a more typical size.
-    bucket_width = 8 # bucket width in pixels
-    thermal_cropped = bucket(
-        thermal_cropped, (1, bucket_width, bucket_width)) / bucket_width**2
-
-    # choose three representative thermal images
-    thermal_first = thermal_cropped[6,:,:]
-    thermal_middle = thermal_cropped[10,:,:]
-    thermal_last = thermal_cropped[28,:,:]
-
-    # combine into single array
-    three_images = np.array([thermal_first, thermal_middle, thermal_last])
-    # get max/min values
-    thermal_max = np.amax(three_images)
-    thermal_min = np.amin(three_images)
-    # scale bar
-    three_images[:, -2:-1, 1:6] = thermal_min
-    
-    
-    # average points around center lobe of the nanodiamond image to get
-    # "average signal level" for darkfield and STE images
-    top = 13
-    bot = top + 48
-    left = 148
-    right = left + 48
-    thermal_signal = (
-        thermal_stack[:,top:bot,left:right].mean(axis=2).mean(axis=1))
-    green_pulse = np.array([
-        0,
-        0,
-        0.95,
-        0.95,
-        0,
-        0,
-        ])
-##    green_pulse = (1 - green_pulse) * np.amin(thermal_signal)
-    dt = 1e-9
-    green_pulse_tail = 5e-5
-    green_pulse_time_axis = np.array([
-        -green_pulse_tail,
-        0-dt,
-        0,
-        green_pulse_duration,
-        green_pulse_duration + dt,
-        green_pulse_duration + green_pulse_tail,
-        ])
-        
-
-    
-    # plot signal v phase
-    max_delay_num = 28
-    fig = plt.figure(figsize = (12.5,4.5))
-    ax = fig.add_subplot(111)
-    lns1 = ax.plot(
-        red_delays[0:max_delay_num]*1e3,
-        thermal_signal[0:max_delay_num],
-        'o-',color='red',
-        label='Delayed phase contrast signal',
-        )
-    lns1b = ax.plot(
-        red_delays[0:max_delay_num]*1e3,
-        bg_signal_change[0:max_delay_num],
-        'o--',color='red',
-        label='Background brightness change',
-        )
-    ax2 = ax.twinx()
-    lns2 = ax2.plot(
-        green_pulse_time_axis*1e3,
-        green_pulse,
-        '-',color='green',
-        label='Excitation laser duration',
-        )
-
-##    lns = lns1 + lns2
-    lns = lns1 + lns1b + lns2
-    labs = [l.get_label() for l in lns]
-    ax.legend(lns, labs, loc='lower right')
-
-    ax.grid()
-    ax.set_xlabel('Time delay between red and green pulses (ms)')
-    ax.set_ylabel('Average pixel count', color='red')
-    ax.tick_params('y', colors='red')
-    ax2.set_ylabel('Laser power (arb. units)', color='green')
-    ax2.tick_params('y', colors='green')
-    ax.set_xlim([-0.25,5.75])
-    ax.set_ylim(-524,24)
-    ax2.set_ylim([-0.05,1.045])
-
-    a = plt.axes([.21, .13, .18, .18])
-    plt.imshow(three_images[0], cmap=plt.cm.gray,
-               interpolation='nearest', vmax=thermal_max, vmin=thermal_min)
-    plt.xticks([])
-    plt.yticks([])
-    a = plt.axes([.33, .36, .18, .18])
-    plt.imshow(three_images[1], cmap=plt.cm.gray,
-               interpolation='nearest', vmax=thermal_max, vmin=thermal_min)
-    plt.xticks([])
-    plt.yticks([])
-    a = plt.axes([.7, .53, .18, .18])
-    plt.imshow(three_images[2], cmap=plt.cm.gray,
-               interpolation='nearest', vmax=thermal_max, vmin=thermal_min)
-    plt.xticks([])
-    plt.yticks([])
-    plt.savefig('./../images/figure_A6/phase_contrast_nd_delayed_signal.svg')
+    plt.figure()
+    for (pow_num,rd_pow) in enumerate(red_powers):
+        plt.errorbar(
+            green_powers,
+            STE_signal_corrected[pow_num,:],
+            yerr=STE_signal_std[pow_num,:]/(200**0.5),
+            label=('Stimulation power = '+str(rd_pow)+' mW'))
+    plt.xlabel('Excitation power (mW)')
+    plt.ylabel('Change in scattered light signal (CMOS pixel counts)')
+    plt.legend(loc='lower left')
+    plt.ylim(-52, 6)
+    plt.xlim(-15, 1465)
+    plt.grid()
+    plt.savefig('./../images/figure_A6/STE_v_green_power.svg')
     plt.show()
-##    plt.close()
-    
+
+    # plot sparse power scan data
+    # all green powers, sparse red powers
+    plt.figure()
+    for (pow_num,rd_pow) in enumerate(red_powers):
+        if pow_num in sparse_red_nums:
+            plt.errorbar(
+                green_powers,
+                STE_signal_corrected[pow_num,:],
+                yerr=STE_signal_std[pow_num,:]/(200**0.5),
+                label=('Stimulation power = '+str(rd_pow)+' mW'))
+    plt.xlabel('Excitation power (mW)')
+    plt.ylabel('Change in scattered light signal (CMOS pixel counts)')
+    plt.legend(loc='lower left')
+    plt.ylim(-40, 6)
+    plt.xlim(-15, 1465)
+    plt.grid()
+    plt.savefig('./../images/figure_A6/STE_v_green_power_sparse.svg')
+    plt.show()
+
+    #all red powers, sparse green powers
+    plt.figure()
+    for (pow_num,gr_pow) in enumerate(green_powers):
+        if pow_num in sparse_green_nums:
+            plt.errorbar(
+                red_powers,
+                STE_signal_corrected[:, pow_num],
+                yerr=STE_signal_std[:, pow_num] / (200 ** 0.5),
+                label=('Excitation power = ' + str(gr_pow) + ' mW'))
+    plt.xlabel('Stimulation power (mW)')
+    plt.ylabel('Change in scattered light signal (CMOS pixel counts)')
+    plt.legend(loc='lower left')
+    plt.xlim(-3, 303)
+    plt.grid()
+    plt.savefig('./../images/figure_A6/STE_v_red_power_sparse.svg')
+    plt.show()
+
+
+    # plot darkfield and stim emission images
+
+    darkfield_image = darkfield_image[0,0:-1,:]
+    STE_delta_image = STE_delta_image[0,0:-1,:]
+    STE_delta_image_corrected = STE_delta_image_corrected[0,0:-1,:]
+
+    # downsample darkfield and STE delta images
+    bucket_width = 8
+    bucket_shape = (bucket_width, bucket_width)
+    darkfield_image = bucket(
+        darkfield_image, bucket_shape) / bucket_width**2
+    STE_delta_image = bucket(
+        STE_delta_image, bucket_shape) / bucket_width**2
+    STE_delta_image_corrected = bucket(
+        STE_delta_image_corrected, bucket_shape) / bucket_width**2
+
+    # scale bar
+    darkfield_image[-2:-1, 1:6] = np.max(darkfield_image)
+    STE_delta_image[-2:-1, 1:6] = np.min(STE_delta_image)
+    STE_delta_image_corrected[-2:-1, 1:6] = np.min(STE_delta_image_corrected)
+
+    fig, (ax0, ax1) = plt.subplots(nrows=1,ncols=2,figsize=(19,5))
+
+    cax0 = ax0.imshow(darkfield_image, cmap=plt.cm.gray,
+                      interpolation='nearest')
+    ax0.axis('off')
+    cbar0 = fig.colorbar(cax0, ax = ax0)
+
+    cax1 = ax1.imshow(STE_delta_image_corrected, cmap=plt.cm.gray,
+                      interpolation='nearest')
+    cbar1 = fig.colorbar(cax1, ax = ax1)
+    ax1.axis('off')
+
+    ax0.text(0.4,2.2,'A',fontsize=72,color='white',fontweight='bold')
+    ax1.text(0.4,2.2,'B',fontsize=72,color='black',fontweight='bold')
+##    plt.savefig('./../images/figure_A6/darkfield_STE_image.svg')
+    plt.show()
 
     return None
 
-def get_bg_level(data):
-    num_regions = 2
-    
-    # region 1
-    bg_up = 2
-    bg_down = 120
-    bg_left = 285
-    bg_right = 379
-    bg_level = data[..., bg_up:bg_down, bg_left:bg_right].mean(axis=(-2, -1))
 
-    # region 2
-    bg_up = 2
-    bg_down = 120
-    bg_left = 1
-    bg_right = 81
-    bg_level += data[..., bg_up:bg_down, bg_left:bg_right].mean(axis=(-2, -1))
-
-    return(bg_level / num_regions)
-
-main()
+if __name__ == '__main__':
+    main()
